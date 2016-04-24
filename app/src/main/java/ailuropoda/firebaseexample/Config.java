45e25c90ae3d2366c9b3a44c468bb975e7cfc653
salvin <mirzaalivas@gmail.com>
2016-04-24 12:40:02 +0600

    firebase app backend example repo

---
+++ app/src/main/java/ailuropoda/firebaseexample/Config.java
@@ -0,0 +1,8 @@
+package ailuropoda.firebaseexample;
+
+/**
+ * Created by salvin on 4/20/2016.
+ */
+public class Config {
+    public static final String FIREBASE_URL = "https://firstandroidapp.firebaseio.com/";
+}
